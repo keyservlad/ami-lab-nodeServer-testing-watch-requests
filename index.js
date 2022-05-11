9fd84d99694a2cf184353733eeec79972c2cc57d
Arnaud Guilhamat <guia2612@usherbrooke.ca>
2022-05-11 11:01:56 -0400

    modif

--- index.js
+++ index.js
@@ -5,11 +5,6 @@ const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-var options = {
-  key: fs.readFileSync("./ssl/server-key.pem"),
-  cert: fs.readFileSync("./ssl/server-cert.pem"),
-};
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -26,6 +21,6 @@ app.post("/", (req, res) => {
   res.send("Welcome post")
 })
 
-var server = https.createServer(options, app).listen(PORT, function () {
-  console.log("Express server listening on port " + PORT);
-});
+app.listen(PORT, function () {
+  console.log(`Express server listening on port ${PORT}`)
+}) 
