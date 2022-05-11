2ac39363bd57dbb4dd99fda1053a593e9753520f
Arnaud Guilhamat <guia2612@usherbrooke.ca>
2022-05-11 10:51:29 -0400

    certificate

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
-const express = require("express")
+const express = require("express");
 
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 
+const PORT = process.env.PORT || 5000;
+const app = express();
 
-const PORT = process.env.PORT || 443
-const app = express()
+var options = {
+  key: fs.readFileSync("./ssl/server-key.pem"),
+  cert: fs.readFileSync("./ssl/server-cert.pem"),
+};
 
-
-
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    console.log(req.body);
-    console.log(JSON.stringify(req.headers), "\n")
-    next();
-  });
+  console.log(req.body);
+  console.log(JSON.stringify(req.headers), "\n");
+  next();
+});
 
-app.listen(PORT, function () {
-  console.log(`Express server listening on port ${PORT}`)
+app.get("/", (req, res) => {
+  res.send("Welcome get")
 })
+app.post("/", (req, res) => {
+  res.send("Welcome post")
+})
+
+var server = https.createServer(options, app).listen(PORT, function () {
+  console.log("Express server listening on port " + PORT);
+});
